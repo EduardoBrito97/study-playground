a96b37cbf121fa778856989118c59458a333fc3f
Eduardo Barreto Brito <eduardobarreto97@gmail.com>
2025-06-04 20:11:49 -0300

    Fixing AnswerRepository custom query

--- study-go-kotlin-spring-boot/src/main/kotlin/com/eduardo/main/repository/AnswerRepository.kt
+++ study-go-kotlin-spring-boot/src/main/kotlin/com/eduardo/main/repository/AnswerRepository.kt
@@ -1,10 +1,14 @@
 package com.eduardo.main.repository
 
 import com.eduardo.main.model.database.Answer
+import jakarta.persistence.NamedQuery
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface AnswerRepository : JpaRepository<Answer, Long> {
-    fun findByTopic(topicId: Long): List<Answer>
+    @Query("select answer from Answer answer where answer.topic.id = :topicId")
+    fun findByTopic(@Param("topicId") topicId: Long): List<Answer>
 }
