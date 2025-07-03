69fd5e507a9d28c4cf17c55af92f26a5a798ec69
Eduardo Barreto Brito <eduardobarreto97@gmail.com>
2025-07-02 21:18:23 -0300

    Setting Pageable as Serializable

--- study-kotlin-spring-boot/src/main/kotlin/com/eduardo/main/AppStartup.kt
+++ study-kotlin-spring-boot/src/main/kotlin/com/eduardo/main/AppStartup.kt
@@ -2,8 +2,11 @@ package com.eduardo.main
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.web.config.EnableSpringDataWebSupport
 
 @SpringBootApplication
+// Enabling pageable serialization
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 class AppStartup()
 
 fun main(args: Array<String>) {
